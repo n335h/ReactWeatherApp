89acbd33ce85dfb62e335745e00d58aa460ee566
Nich H <nmjhorishny@gmail.com>
2023-04-13 23:06:32 +0100

    Weather updates symbols

--- src/App.js
+++ src/App.js
@@ -3,15 +3,12 @@ import axios from 'axios'
 
 
 function App() {
-
-
   const [data, setData] = useState({})
-  const [location, setLocation] = useState("")
-  const API_KEY = (`57a034c92e9e4ba3b3d66b3b8af80d87`)
+  const [location, setLocation] = useState('')
+  const [description, setDescription] = useState('')
+  const API_KEY = '57a034c92e9e4ba3b3d66b3b8af80d87'
 
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}&units=metric`
-
- 
 
   
 
@@ -19,6 +16,7 @@ function App() {
     if (event.key === 'Enter') {
       axios.get(url).then((response) => {
       setData(response.data)
+      setDescription(response.data.weather[0].description)
       console.log (response.data)
       })
     }
@@ -29,7 +27,7 @@ function App() {
   <div className ="app">
 
     <div className="search">
-    <input
+    <input 
     value={location}
     onChange={event => setLocation(event.target.value)}
     onKeyPress={searchLocation}
@@ -41,16 +39,33 @@ function App() {
       <div className="container">
       <div className="top">
       <div className="location">
-      <p>{data.name}</p>
-      </div>
+      <span>{data.name}  {data.sys?.country}</span>
+        </div>
       <div className="temp">
         {data.main ? <h1>{data.main.temp}&deg;C</h1>: null}
+      </div>
 
+      <div className="minmax">
+        {data.main ? <p> Min {data.main.temp_min}&deg;C / Max {data.main.temp_max}&deg;C</p>: null}
       </div>
       <div className="description">
-        {data.weather ? <p>{data.weather[0].main}</p>: null}
-      </div>
-      </div>
+          {description && (
+            <div>
+              {data.weather[0]?.icon && (
+                <img
+                  src={`http://openweathermap.org/img/wn/${data.weather[0].icon}.png`}
+                  alt={data.weather[0].description}
+                />
+              )}
+              <p>{description}</p>
+            </div>
+          )}
+        </div>
+
+
+       
+    
+        </div>
       <div className="bottom">
       <div className="feels">
         {data.main ?  <p className="bold">{data.main.feels_like}&deg;C</p> : null}
