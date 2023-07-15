c5e33555dc49ba0f2cd835865250ed97736d6b31
Nich H <nmjhorishny@gmail.com>
2023-07-15 10:14:27 +0100

    toggle theme icons

--- src/Components/ThemeToggle/index.js
+++ src/Components/ThemeToggle/index.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React from 'react';
+import sun from '../../assets/sun.png';
+import moon from '../../assets/moon.png';
 
 function ThemeToggle({ theme, toggleTheme }) {
   return (
-    <label className="switch">
-      <input type="checkbox" className="Theme" checked={theme === "dark"} onChange={toggleTheme} />
-      <span className="slider round"></span>
-      {theme === "light" }
-    </label>
+    <div className="ThemeToggle">
+      <label className="switch">
+        <div className="toggleSwitch">
+          <input
+            type="checkbox"
+            className="Theme"
+            checked={theme === 'dark'}
+            onChange={toggleTheme}
+          />
+          <span className="slider round"></span>
+        </div>
+      </label>
+      <div className="toggleText">
+        {theme === 'dark' ? (
+          <img id="themeicon" className="sun" src={sun} alt="" />
+        ) : (
+          <img id="themeicon" className="moon" src={moon} alt="" />
+        )}
+      </div>
+    </div>
   );
 }
 
