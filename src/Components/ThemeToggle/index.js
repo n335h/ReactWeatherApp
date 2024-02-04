b96fe8a838e5943ab52627f3897d80bd4992fcd9
Nich H <nmjhorishny@gmail.com>
2024-02-04 10:03:19 +0000

    tidied up uneeded css

--- src/Components/ThemeToggle/index.js
+++ src/Components/ThemeToggle/index.js
@@ -1,30 +1,41 @@
 import React from 'react';
 import sun from '../../assets/sun.png';
 import moon from '../../assets/moon.png';
+import './style.css';
 
 function ThemeToggle({ theme, toggleTheme }) {
-  return (
-    <div className="ThemeToggle">
-      <label className="switch">
-        <div className="toggleSwitch">
-          <input
-            type="checkbox"
-            className="Theme"
-            checked={theme === 'dark'}
-            onChange={toggleTheme}
-          />
-          <span className="slider round"></span>
-        </div>
-      </label>
-      <div className="toggleText">
-        {theme === 'dark' ? (
-          <img id="themeicon" className="sun" src={sun} alt="" />
-        ) : (
-          <img id="themeicon" className="moon" src={moon} alt="" />
-        )}
-      </div>
-    </div>
-  );
+	return (
+		<div className='ThemeToggle'>
+			<label className='switch'>
+				<div className='toggleSwitch'>
+					<input
+						type='checkbox'
+						className='Theme'
+						checked={theme === 'dark'}
+						onChange={toggleTheme}
+					/>
+					<span className='slider round'></span>
+				</div>
+			</label>
+			<div className='toggleText'>
+				{theme === 'dark' ? (
+					<img
+						id='themeicon'
+						className='sun'
+						src={sun}
+						alt=''
+					/>
+				) : (
+					<img
+						id='themeicon'
+						className='moon'
+						src={moon}
+						alt=''
+					/>
+				)}
+			</div>
+		</div>
+	);
 }
 
 export default ThemeToggle;
